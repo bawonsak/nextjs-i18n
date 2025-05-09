c858a37fe951d80706af73d1b5b40fc7200962df
joe <jojoe.bawonsak@gmail.com>
2025-05-09 22:02:14 +0700

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,7 @@ const Page = () => {
     <div className="flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl">{t("HomePage.title")}</h1>
       <p className="text-2xl">{t("HomePage.description")}</p>
+      <p className="text-2xl">{t("price", { price: 100 })}</p>
       <Link href="/about" className="mt-4 text-blue-500 underline">
         {t("Menu.about")}
       </Link>
